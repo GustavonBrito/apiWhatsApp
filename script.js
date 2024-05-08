983cb93db427666b7052daa93365b9f98091ae7a
GustavonBrito <gustavoserafio@gmail.com>
2024-05-08 00:53:30 -0300

    teste para intergração com wpp

---
+++ script.js
@@ -0,0 +1,14 @@
+const header = document.getElementById('header');
+const main = document.getElementById('main');
+const footer = document.getElementById('footer');
+
+header.addEventListener('click', () => {
+    console.log('header')
+})
+main.addEventListener('click', () => {
+    console.log('main')
+})
+footer.addEventListener('click', () => {
+    console.log('footer')
+})
+
